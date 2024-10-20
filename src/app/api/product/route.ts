d0186ce4bb744cde7205f3b13b89046e83dd4c6b
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-10-20 20:47:15 +0200

    the application is working again

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -2,39 +2,85 @@ import mysql from "mysql2/promise";
 import { NextRequest, NextResponse } from "next/server";
 
 //Create the connection to database
-function getDB() {
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "dawid132009",
-    database: "shoppinglistdb",
-    port: 3306,
-  });
+async function getDB() {
+  try {
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "dawid132009",
+      database: "shoppinglistdb",
+      port: 3306,
+    });
+    console.log("Database connection created");
+    return connection;
+  } catch (error) {
+    console.log("Error connecting to database", error);
+    return null;
+  }
 }
 
 // GET method
 export async function GET() {
   const connection = await getDB();
-  const [results] = await connection.query("SELECT * FROM products;");
-  return NextResponse.json(results);
+  if (!connection) {
+    return NextResponse.json(
+      { error: "cant connect with database" },
+      { status: 500 }
+    );
+  }
+  try {
+    const [results] = await connection.query("SELECT * FROM products;");
+    return NextResponse.json(results);
+  } catch (error) {
+    console.log("cant use get method", error);
+    return NextResponse.json({ error: "cant use get method" }, { status: 500 });
+  }
 }
 
 // POST method
 export async function POST(req: NextRequest) {
   const connection = await getDB();
-  const body = await req.json();
-  const [results] = await connection.query(
-    `INSERT INTO products (name) VALUES ("${body.name}");`
-  );
-  return NextResponse.json({}, { status: 201 });
+  if (!connection) {
+    return NextResponse.json(
+      { error: "cant connect with database" },
+      { status: 500 }
+    );
+  }
+  try {
+    const body = await req.json();
+    const [results] = await connection.query(
+      `INSERT INTO products (name) VALUES ("${body.name}");`
+    );
+    return NextResponse.json({}, { status: 201 });
+  } catch (error) {
+    console.log("cant use post method", error);
+    return NextResponse.json(
+      { error: "cant use post method" },
+      { status: 500 }
+    );
+  }
 }
 
 //DELETE method
 export async function DELETE(req: NextRequest) {
   const connection = await getDB();
-  const body = await req.json();
-  const [results] = await connection.query(
-    `DELETE FROM products WHERE id = ${body.id};`
-  );
-  return NextResponse.json(results);
+  if (!connection) {
+    return NextResponse.json(
+      { error: "cant connect with database" },
+      { status: 500 }
+    );
+  }
+  try {
+    const body = await req.json();
+    const [results] = await connection.query(
+      `DELETE FROM products WHERE id = ${body.id};`
+    );
+    return NextResponse.json(results);
+  } catch (error) {
+    console.log("cant use delete method", error);
+    return NextResponse.json(
+      { error: "cant use delete method" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/components/list-element.tsx
+++ src/app/components/list-element.tsx
@@ -2,12 +2,12 @@ import { Product } from "../product";
 import { useState } from "react";
 
 export function ListElement({ product }: { product: Product }) {
-  const [productList, setProductList] = useState<Product[]>([]);
-  async function fetchProducts() {
-    const response = await fetch("api/products");
-    const body = await response.json();
-    setProductList(body);
-  }
+  // const [productList, setProductList] = useState<Product[]>([]);
+  // async function fetchProducts() {
+  //   const response = await fetch("api/products");
+  //   const body = await response.json();
+  //   setProductList(body);
+  // }
 
   async function deleteTask(id: number) {
     const response = await fetch("api/product", {
