ea8a4b9a5a1244c95192b573a8889ff76db85384
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-10-24 19:32:46 +0200

    icons added

--- src/app/components/list-element.tsx
+++ src/app/components/list-element.tsx
@@ -1,4 +1,5 @@
 import { Product } from "../product";
+import { useState } from "react";
 
 export function ListElement({
   product,
@@ -7,6 +8,9 @@ export function ListElement({
   product: Product;
   fetchProducts: () => Promise<void>;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
   async function deleteTask(id: number) {
     const response = await fetch("api/product", {
       method: "DELETE",
@@ -17,15 +21,28 @@ export function ListElement({
     fetchProducts();
   }
 
-  return (
+  function inputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
+  return !isEditing ? (
     <div className="listElement">
       <div>{product.name}</div>
       <div className="buttons">
-        <button>
+        <button onClick={() => setIsEditing(true)}>
           <i className="fa-solid fa-pen-to-square icon"></i>
         </button>
         <button onClick={() => deleteTask(product.id)} className="">
           <i className="fa-solid fa-trash-can icon"></i>
+        </button>
+      </div>
+    </div>
+  ) : (
+    <div className="listElement">
+      <input value={product.name} onChange={inputChange} />
+      <div className="buttons !justify-end">
+        <button>
+          <i className="fa-solid fa-square-check icon"></i>
         </button>
       </div>
     </div>
